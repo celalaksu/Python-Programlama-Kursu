915b236dbeadb21dae4b7ba7ad0a70f1b6b62283
Celal AKSU <32665644+celalaksu@users.noreply.github.com>
2022-08-16 21:17:44 +0300

    Add files via upload

---
+++ python_kursu/bolum_2_oop/11_gun/de2.py
@@ -0,0 +1,8 @@
+dict ={}
+l = ['a','b','c','d']
+
+
+def fun(x,y,z):
+    return x
+
+print(fun(0,z=1,y=3))
---
+++ python_kursu/bolum_2_oop/11_gun/deneme.py
@@ -0,0 +1,9 @@
+def fonk():
+    return
+
+print(fonk())
+
+tup = (1,2,3,4)
+tup = tup[1:-1]
+tup = tup[0]
+print(tup)
